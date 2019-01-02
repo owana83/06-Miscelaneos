fdce5c1caac24199b852b3244547a81f860611c5
owana83 <frafalriv@gmail.com>
2019-01-02 17:51:31 +0100

    principio de practica miscelanea de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { CssComponent } from './components/css/css.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgStyleComponent,
+    CssComponent
   ],
   imports: [
     BrowserModule,
